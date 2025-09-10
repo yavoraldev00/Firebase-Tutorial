ceca9328e6b59ddbfe58ffcc1bf08f547a48d918
YAVOR ALDEV <y.aldev@student.fontys.nl>
2025-09-10 11:06:08 +0200

    Lesson 3 - Setting up Firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from index.js")
+import { firebaseConfig } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXNfybclkDYxSlA1dTFj1TUZYPhWR10d0",
+  authDomain: "fir-9-tutorial-48d10.firebaseapp.com",
+  projectId: "fir-9-tutorial-48d10",
+  storageBucket: "fir-9-tutorial-48d10.firebasestorage.app",
+  messagingSenderId: "374619808371",
+  appId: "1:374619808371:web:4cc1143be3d10f5c1d8006"
+};
+
+InitializeApp(firebaseConfig)
